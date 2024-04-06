3c454a30ebe8e38b170f6ded218fbdffb0d6bb25
srinivas gonepudi <srigonepudi@gmail.com>
2024-04-06 21:00:00 +0530

    added

--- vpc_testing/parameter.tf
+++ vpc_testing/parameter.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "vpc_id" {
     name = "${var.application_name}-${var.enviornment}/vpc_id"
     type = "String"
-    value = module.roboshop.vpc_id
+    value = module.vpc_roboshop.vpc_id
 }
 
